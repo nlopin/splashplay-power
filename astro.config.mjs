a1ac61d923d911b52daa4233e9e9d1dad25b89fa
Nikolai Lopin <code@lopin.me>
2025-11-11 17:33:44 +0100

    chore: add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +12,17 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+  env: {
+    schema: {
+      STRIPE_SECRET_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      CALENDLY_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+  adapter: netlify(),
 });
