1cbf103e4101044e65f584f92f3cf58175a2e9db
Nikolai Lopin <code@lopin.me>
2026-01-04 20:47:25 -0600

    add robots.txt and sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, envField } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -67,5 +68,5 @@ export default defineConfig({
   },
 
   adapter: netlify(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
