7d0890f07fd25de0526ca17b41d1a2c4d66ae51d
Nikolai Lopin <code@lopin.me>
2026-01-07 22:53:50 -0600

    update website name

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://splashplay.netlify.app",
+  site: "https://splashplay.es",
 
   i18n: {
     defaultLocale: "es",
